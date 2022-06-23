5a1f694c95be455624a777659d3eab731c0cd366
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-06-23 16:59:18 +0200

    Ändere (php_datenbank/www/getDemoData.php php_get_post_20220623/getallpointofintrest.php php_get_post_20220623/getpointofintrest.php php_get_post_20220623/index.php ) Füg hin (php_datenbank/www/.getDemoData.php.swp php_get_post_20220623/deploy.sh php_get_post_20220623/www/getDemoData.php php_get_post_20220623/www/getallpointofintrest.php php_get_post_20220623/www/getpointofintrest.php php_get_post_20220623/www/index.php )

--- php_get_post_20220623/www/getDemoData.php
+++ php_get_post_20220623/www/getDemoData.php
@@ -3,6 +3,7 @@
 //erfkarimi
 //2022-06-16_16:12:01
 //Thema:
+//
 require_once("../private/dbconnection.inc.php");
 #echo "$user $host $database\n";
 $conn = mysqli_connect($host, $user, $password, $database);
@@ -15,12 +16,12 @@ if (!$conn) {
 $sql = "select * from demo";
 $result = mysqli_query($conn, $sql);
 if ($result){
- # header("Content-Type: text/html");
+  header("Content-Type: text/html");
   #(cat html/head.html)
 }
 while($row = mysqli_fetch_assoc($result)) {
   echo $row['id']." ".$row['name']."<br>\n";
 }
 
-#cat html/foot.html
+cat html/foot.html
 mysqli_close($conn);
